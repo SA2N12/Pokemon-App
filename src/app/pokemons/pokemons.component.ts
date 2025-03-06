283e1a881e2496c73e6e0d08f8720d0caf0660af
Jules <lecorrejules10@gmail.com>
2025-03-06 11:34:44 +0100

    tp-3

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,11 +1,5 @@
-import { Component } from '@angular/core';
-
-interface Pokemon {
-  name: string;
-  type: string;
-  size: string;
-  weight: string;
-}
+import { Component, Inject } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemons',
@@ -14,42 +8,11 @@ interface Pokemon {
   styleUrl: './pokemons.component.css'
 })
 export class PokemonsComponent {
-  pokemons: Pokemon[] = [
-    {
-      name : "Bulbizarre",
-      type : "plante, poison",
-      size : "70cm",
-      weight : "6,9kg"
-    },
-    {
-      name : "Herbizarre",
-      type : "plante, poison",
-      size : "1m",
-      weight : "13kg"
-    },
-    {
-      name : "Florizarre",
-      type : "plante, poison",
-      size : "2m",
-      weight : "100kg"
-    },
-    {
-      name : "Salamèche",
-      type : "feu",
-      size : "60cm",
-      weight : "8,5kg"
-    },
-    {
-      name : "Reptincel",
-      type : "feu",
-      size : "1,1m",
-      weight : "19kg"
-    },
-    {
-      name : "Dracaufeu",
-      type : "feu",
-      size : "1,7m",
-      weight : "90,5kg"
-    }
-  ]
+
+  pokemons: any[] = [];
+
+  constructor(@Inject(PokemonService) private pokemonService : PokemonService){
+    this.pokemons = pokemonService.pokemons;
+  }
+
 }
