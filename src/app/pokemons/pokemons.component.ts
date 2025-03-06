a1ef0112211b128597e779d33080256ba87fa750
Jules <lecorrejules10@gmail.com>
2025-03-06 10:20:21 +0100

    tp-2

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Pokemon {
+  name: string;
+  type: string;
+  size: string;
+  weight: string;
+}
+
 @Component({
   selector: 'app-pokemons',
   imports: [],
@@ -7,5 +14,42 @@ import { Component } from '@angular/core';
   styleUrl: './pokemons.component.css'
 })
 export class PokemonsComponent {
-
+  pokemons: Pokemon[] = [
+    {
+      name : "Bulbizarre",
+      type : "plante, poison",
+      size : "70cm",
+      weight : "6,9kg"
+    },
+    {
+      name : "Herbizarre",
+      type : "plante, poison",
+      size : "1m",
+      weight : "13kg"
+    },
+    {
+      name : "Florizarre",
+      type : "plante, poison",
+      size : "2m",
+      weight : "100kg"
+    },
+    {
+      name : "Salamèche",
+      type : "feu",
+      size : "60cm",
+      weight : "8,5kg"
+    },
+    {
+      name : "Reptincel",
+      type : "feu",
+      size : "1,1m",
+      weight : "19kg"
+    },
+    {
+      name : "Dracaufeu",
+      type : "feu",
+      size : "1,7m",
+      weight : "90,5kg"
+    }
+  ]
 }
